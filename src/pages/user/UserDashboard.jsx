c3b2b40e978de979794b5bdf596e6f70368f63f4
Mohammad Abbas <msaabbas6@gmail.com>
2026-04-09 11:39:56 +0200

    first commit

--- src/pages/user/UserDashboard.jsx
+++ src/pages/user/UserDashboard.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import styles from '../../styles/dashboard/dashboard.module.scss'
 import ViewAccounts from '../../components/Dashboard/ViewAccounts'
 import ViewTransations from '../../components/Dashboard/ViewTransations'
+import Charts from '../../components/Dashboard/Charts'
 
 const UserDashboard = () => {
   return (
     <div className={styles.dashboardContainer}>
+      <div className={styles.leftSide}>
       <ViewAccounts />
       <ViewTransations />
+      </div>
+      <div className={styles.rightSide}>
+        <Charts />
+      </div>
     </div>
   )
 }
