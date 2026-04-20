2e6e29f46a371bfb362115bf06a7179068957982
Mohammad Abbas <msaabbas6@gmail.com>
2026-04-20 08:36:10 +0200

    update the design  of account info

--- src/components/accounts/AccountsInfo.jsx
+++ src/components/accounts/AccountsInfo.jsx
@@ -3,19 +3,20 @@ import styles from '../../styles/components/accounts/accountsinfo.module.scss';
 import React from 'react'
 
 const AccountsInfo = ({accounts}) => {
+  console.log(accounts)
   return (
     <div className={styles.infoContainer}>
       <div className={styles.infoItem}>
-        <span>Total Balance:</span>
-        <span>${accounts?.totalBalance}</span>
+        <span className={styles.title}>Total Balance:</span>
+        <span className={styles.value}>${accounts?.totalBalance}</span>
       </div>
       <div className={styles.infoItem}>
-        <span>Number of Accounts:</span>
-        <span>{accounts?.accounts?.length || 0}</span>
+        <span className={styles.title}>Number of Accounts:</span>
+        <span className={styles.value}>{accounts?.accounts?.length || 0}</span>
       </div>
       <div className={styles.infoItem}>
-        <span>Last Updated:</span>
-        <span>June 20, 2024</span>
+        <span className={styles.title}>Last Updated:</span>
+        <span className={styles.value}>June 20, 2024</span>
       </div>
     </div>
   )
